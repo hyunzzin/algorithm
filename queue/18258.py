f196afe6dab55d36861f810e3a5f7a00be6a4dbb
hyunzzin <forinpea121@gmail.com>
2021-10-28 22:31:03 +0900

    Feat : add 18258 queue ( reduce time )

--- queue/18258.py
+++ queue/18258.py
@@ -1,35 +1,34 @@
 # deque 모듈 없이 queue 구현
-import sys
+from sys import stdin
 queue = []
-n = int(sys.stdin.readline())
+n = int(stdin.readline())
 st = 0
-bk = 0
+
 for _ in range(n):
-    cmd = list(map(str, sys.stdin.readline().split()))
+    cmd = list(map(str,stdin.readline().split()))
     if cmd[0] == 'push':
         queue.append(cmd[1])
-        bk+=1
     elif cmd[0] == 'pop':
-        if st == bk:
+        if st == len(queue):
             print(-1)
             continue
         print(queue[st])
         st+=1
     elif cmd[0] == 'size':
-        print(bk-st)
+        print(len(queue)-st)
     elif cmd[0] == 'empty':
-        if st == bk:
+        if st == len(queue):
             print(1)
         else:
             print(0)
     elif cmd[0] == 'front':
-        if st == bk:
+        if st == len(queue):
             print(-1)
         else:
             print(queue[st])
     elif cmd[0] == 'back':
-        if st == bk:
+        if st == len(queue):
             print(-1)
             continue
         else:
-            print(queue[bk-1])
+            print(queue[-1])
