298ee0a0a761a80b74a8acc2c373e49f920c2bfa
hyunzzin <forinpea121@gmail.com>
2021-10-29 15:08:35 +0900

    Feat : add 2164 queue

---
+++ queue/2164.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+result = [i for i in range(1,n+1)]
+print(result)
+st = 0
+while len(result)-st > 1:
+    st+=1
+    result.append(result[st])
+    st+=1
+print(result[st])
