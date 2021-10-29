3d1dc67245a573e5e2d13369d21105cac06f7203
hyunzzin <forinpea121@gmail.com>
2021-10-29 14:33:11 +0900

    Feat : modified 10828 stack, 18258 queue

--- queue/18258.py
+++ queue/18258.py
@@ -1,32 +1,37 @@
 # deque 모듈 없이 queue 구현
+"""
+파이썬 3.5에서 32비트 정수는 28바이트를 소비한다.
+그에 비해 문자열은 50바이트를 소비한다.
+따라서 push를 할 때 문자형태의 숫자라면 정수형으로 변환함으로써 메모리를 적게 사용할 수 있다.
+"""
 from sys import stdin
+
 queue = []
 n = int(stdin.readline())
 st = 0
-
 for _ in range(n):
-    cmd = list(map(str,stdin.readline().split()))
-    if cmd[0] == 'push':
-        queue.append(cmd[1])
-    elif cmd[0] == 'pop':
+    cmd = stdin.readline().split()
+    if cmd[0] == "push":
+        queue.append(int(cmd[1]))  # 메모리 120208kb -> 91056kb 절감
+    elif cmd[0] == "pop":
         if st == len(queue):
             print(-1)
             continue
         print(queue[st])
-        st+=1
-    elif cmd[0] == 'size':
-        print(len(queue)-st)
-    elif cmd[0] == 'empty':
+        st += 1
+    elif cmd[0] == "size":
+        print(len(queue) - st)
+    elif cmd[0] == "empty":
         if st == len(queue):
             print(1)
         else:
             print(0)
-    elif cmd[0] == 'front':
+    elif cmd[0] == "front":
         if st == len(queue):
             print(-1)
         else:
             print(queue[st])
-    elif cmd[0] == 'back':
+    elif cmd[0] == "back":
         if st == len(queue):
             print(-1)
             continue
--- stack/10828.py
+++ stack/10828.py
@@ -1,4 +1,4 @@
-'''
+"""
 스택 구현하기
 명령
 push X: 정수 X를 스택에 넣는 연산이다.
@@ -11,36 +11,37 @@ top: 스택의 가장 위에 있는 정수를 출력한다.
 
 명령의 수 N(1<=N<=10,000)
 정수 k (1<=k<=100,000)
-'''
+"""
 import sys
+
 n = int(sys.stdin.readline())
 idx = 0
 result = [0 for _ in range(10000)]
 for _ in range(n):
     cmd = list(map(str, sys.stdin.readline().split()))
     # push
-    if cmd[0] == 'push':
-        result[idx]= int(cmd[1])
-        idx+=1
+    if cmd[0] == "push":
+        result[idx] = int(cmd[1])
+        idx += 1
     # pop
-    elif cmd[0] == 'pop':
+    elif cmd[0] == "pop":
         if bool(idx):
-            print(result[idx-1])
-            idx-=1
+            print(result[idx - 1])
+            idx -= 1
         else:
             print(-1)
     # size
-    elif cmd[0] == 'size':
+    elif cmd[0] == "size":
         print(idx)
     # empty
-    elif cmd[0] == 'empty':
+    elif cmd[0] == "empty":
         if bool(idx):
             print(0)
         else:
             print(1)
     # top
-    elif cmd[0] == 'top':
+    elif cmd[0] == "top":
         if bool(idx):
-            print(result[idx-1])
+            print(result[idx - 1])
         else:
             print(-1)
