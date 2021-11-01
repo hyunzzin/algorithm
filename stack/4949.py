3f5dfdbf3bdf6c4478dc94602c133e85779c9b18
hyunzzin <forinpea121@gmail.com>
2021-11-01 18:05:47 +0900

    Feat : modified 4949 stack

--- stack/4949.py
+++ stack/4949.py
@@ -1,32 +1,32 @@
 import sys
 
 while True:
-    S = sys.stdin.readline().strip('\n')
-    if S == '.':
+    S = sys.stdin.readline().strip("\n")
+    if S == ".":
         break
     isvalid = True
     stack = []
     answer = 0
     for s in S:
-        if s == '(' or s == '[':
+        if s == "(" or s == "[":
             stack.append(s)
-        elif (s == ')'or s == ']') and not stack:
+        elif (s == ")" or s == "]") and not stack:
             isvalid = False
             break
-        elif s == ')':
-            if stack[-1] == '(':
+        elif s == ")":
+            if stack[-1] == "(":
                 stack.pop()
             else:
                 isvalid = False
                 break
-            
-        elif s == ']':
-            if stack[-1] == '[':
+
+        elif s == "]":
+            if stack[-1] == "[":
                 stack.pop()
             else:
                 isvalid = False
                 break
     if isvalid and not stack:
-        print('yes')
+        print("yes")
     else:
-        print('no')
+        print("no")
