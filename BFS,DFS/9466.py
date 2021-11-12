41eb44f0181e826b16da6eb024d2ff904c736527
hyunzzin <forinpea121@gmail.com>
2021-11-12 16:17:15 +0900

    Feat : add 9466 DFS

--- BFS,DFS/9466.py
+++ BFS,DFS/9466.py
@@ -6,23 +6,42 @@
 어느 팀에도 속하지 않는 학생들의 수 1:58
 '''
 import sys
-from collections import deque
-vis_check = [False for _ in range(100002)]
-Done = [False for _ in range(100002)]
-T = int(sys.stdin.readline())
+sys.setrecursionlimit(10**8)
+T = int(sys.stdin.readline().strip('\n'))
+
+def DFS(i, cnt):
+    nxt_num = choice[i]
+    vis_check[i] = True
+
+    if not vis_check[nxt_num]:
+        cnt = DFS(nxt_num, cnt)
+    else:
+        if not Done[nxt_num]:
+            j = nxt_num
+            while j != i:
+                j = choice[j]
+                cnt+=1
+                
+            cnt+=1
+    Done[i] = True
+    return cnt
+
 for _ in range(T):
     stu = int(sys.stdin.readline())
-    choice = list(map(int, sys.stdin.readline().split()))
-    # 자기 자신을 선택하는 경우
-    # 다른 사람을 선택하는 경우
+    vis_check = [False for _ in range(stu+2)]
+    Done = [False for _ in range(stu+2)]
+    choice = [0]+list(map(int, sys.stdin.readline().split()))
     cnt = 0
-    for i in range(stu):
+    for i in range(1,stu+1):
         if not vis_check[i]:
-            DFS()
+            
+            cnt = DFS(i, cnt)
+
+    print(stu-cnt)
             
         
     
 
-def BFS():
-    if visit_check[]
+
+        
 
