7b50913007d1ec0cca34a3056a2ce53fb742038b
hyunzzin <forinpea121@gmail.com>
2021-12-06 22:29:09 +0900

    Feat : add 11052 dynamic programming

--- dynamic_programming/11052.py
+++ dynamic_programming/11052.py
@@ -4,4 +4,20 @@
 '''
 import sys
 input = sys.stdin.readline
-n = int(input())
+N = int(input())
+arr = list(map(int, input().split()))
+memo = [0 for _ in range(N+1)]
+def card(N):
+    if N == 1:
+        return
+    for i in range(1,N+1):
+        sumN  = arr[0]
+        for j in range(i//2+1):
+            temp = memo[j]+memo[i-j]
+            if j == 0:
+                temp = arr[i-1]
+            sumN = max(sumN, temp)
+        memo[i] = sumN
+    print(memo[-1])
+    
+card(N)
