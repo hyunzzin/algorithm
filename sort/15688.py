ab679994268eed712f60c32279014c8ec653d644
hyunzzin <forinpea121@gmail.com>
2022-01-07 14:03:27 +0900

    Feat : midified 15688 sort

--- sort/15688.py
+++ sort/15688.py
@@ -1,9 +1,7 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(int(input()))
+arr = [int(input()) for i in range(n)]
 
 # merge sort를 구현하는 것보다 sort 메소드가 더 빠름
 arr.sort()
